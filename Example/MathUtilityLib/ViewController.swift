b059903e1636a133400e40a79f679ab3495a9398
100699 <priti.pallabi0699@gmail.com>
2022-02-16 14:40:19 +0530

    Initial commit

---
+++ Example/MathUtilityLib/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MathUtilityLib
+//
+//  Created by 100699 on 02/16/2022.
+//  Copyright (c) 2022 100699. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
